ef77cce49f709cf911198c6b4618f5e924d53e57
Charise Arter <mine@Charises-MacBook-Pro.local>
2020-06-04 22:43:06 -0500

    got most of test to work but eamil part is being strange

--- src/components/ContactForm.test.js
+++ src/components/ContactForm.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import ContactForm from './ContactForm';
 
 // test('title of test', () => {// do something})
@@ -8,4 +8,45 @@ test('ContactForm renders without crashing', () => {
   render(<ContactForm />);
 })
 
-//Check to see if COntact Form has everything
+//Check to see if labels are correct
+test('if user can type in input fields', () => {
+  //Arrange - render where I want to check and set up variables of what I want checked
+  const { getByText, findAllByText , getByPlaceholderText } = render(<ContactForm />);
+ 
+  //query by form inputs
+  const firstNameInput = getByPlaceholderText(/edd/i);
+  const lastNameInput = getByPlaceholderText(/burke/i);
+  const emailInput = getByPlaceholderText(/bluebill1049@hotmail.com/i);
+
+
+  //Act - check to see user can type
+  //fireEvent to type in inputs
+//firEven.change(variable from query inputs)
+  fireEvent.change(firstNameInput, {
+    //target name: i'nputNameofField', value: 'whatever' 
+    target: { name: 'firstName', value: 'Ben' }
+  });
+
+  fireEvent.change(lastNameInput, {
+    target: { name: 'lastName', value: 'Tennington' }
+  });
+  
+  fireEvent.change(emailInput, {
+    target: { name: 'email', value: 'benn@ten.com' }
+  });
+
+  
+
+  console.log(firstNameInput.value);
+
+  //query for submit button
+  const submitButton = getByText(/submit/i);
+
+  //fireEvent to click button
+  fireEvent.click(submitButton);
+
+  //Assert - what I expect to happen
+  findAllByText(/ben/i);
+  expect(submitButton).toBeInTheDocument();
+  
+})
